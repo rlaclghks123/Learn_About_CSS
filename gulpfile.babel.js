430618749778d886b26dbf1a06f3536757f8ff9a
Chi Hwan Kim <rlaclghks123@naver.com>
2022-04-23 11:00:23 +0900

    cloneCording -schwartzmedia -1
    set up

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,7 +9,7 @@ sass.compiler = require("node-sass");
 const routes = {
     css: {
         watch: "src/scss/*",
-        src: "src/scss/zoo.scss",
+        src: "src/scss/schwartzmedia.scss",
         dest: "dest/css"
     }
 };
