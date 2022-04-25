ac3b76e489ce3040c27780ea00544f0b73497316
Chi Hwan Kim <rlaclghks123@naver.com>
2022-04-25 18:45:18 +0900

    cloneCording -tolv -1
    set up

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,7 +9,7 @@ sass.compiler = require("node-sass");
 const routes = {
     css: {
         watch: "src/scss/*",
-        src: "src/scss/schwartzmedia.scss",
+        src: "src/scss/tolv.scss",
         dest: "dest/css"
     }
 };
