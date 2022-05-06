2e2d2b4119b5bd98fe6fb606d543d5ca3ad5d78a
Chi Hwan Kim <rlaclghks123@naver.com>
2022-05-06 18:31:01 +0900

    cloneCording -beige
    마지막 수정.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,7 +9,7 @@ sass.compiler = require("node-sass");
 const routes = {
     css: {
         watch: "src/scss/*",
-        src: "src/scss/rodic.scss",
+        src: "src/scss/beige.scss",
         dest: "dest/css"
     }
 };
